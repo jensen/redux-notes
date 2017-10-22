e0575bcbc7225c9e2c196023d491c0cb59bd5abc
Karl Jensen <karljensen@gmail.com>
2017-10-21 18:48:28 -0700

    Can set user name and color.

--- client/src/redux/user.js
+++ client/src/redux/user.js
@@ -1,1 +1,41 @@
-export default (state = {}, action) => state;
+const USER_SET_NAME = 'USER_SET_NAME';
+const USER_SET_COLOR = 'USER_SET_COLOR';
+
+export function setName(name) {
+  return {
+    type: USER_SET_NAME,
+    name
+  };
+}
+
+export function setColor(color) {
+  return {
+    type: USER_SET_COLOR,
+    color
+  };
+}
+
+const initialState = {
+  name: '',
+  color: '#fff'
+};
+
+export default (state = initialState, action) => {
+  const { type, name, color } = action;
+
+  if(type === USER_SET_NAME) {
+    return {
+      ...state,
+      name
+    };
+  }
+
+  if(type === USER_SET_COLOR) {
+    return {
+      ...state,
+      color
+    };
+  }
+
+  return state;
+};
