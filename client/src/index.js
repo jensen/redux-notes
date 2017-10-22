838856fc06acb1e5a393fa818b3e2d1d169bbc2c
Karl Jensen <karljensen@gmail.com>
2017-10-21 18:29:23 -0700

    Add redux-devtools-extension.

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from 'app/redux/';
 
 import Root from 'app/containers/Root.jsx';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 const Application = () => (
   <Provider store={ store }>
