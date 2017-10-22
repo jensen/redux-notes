b0f1e5eb31fac04c2b592596964ef8149ceb130a
Karl Jensen <karljensen@gmail.com>
2017-10-21 19:56:54 -0700

    Add reducer and actions for chat.
    - addMessage, addNotification, setUserCount

--- client/src/redux/chat.js
+++ client/src/redux/chat.js
@@ -1,1 +1,65 @@
-export default (state = {}, action) => state;
+const CHAT_ADD_MESSAGE = 'CHAT_ADD_MESSAGE';
+const CHAT_ADD_NOTIFICATION = 'CHAT_ADD_NOTIFICATION';
+const CHAT_SET_USER_COUNT = 'CHAT_SET_USER_ACCOUNT';
+
+export function addMessage(username, content) {
+  return {
+    type: CHAT_ADD_MESSAGE,
+    username,
+    content
+  };
+}
+
+export function addNotification(content) {
+  return {
+    type: CHAT_ADD_NOTIFICATION,
+    content
+  };
+}
+
+export function setUserCount(count) {
+  return {
+    type: CHAT_SET_USER_COUNT,
+    count
+  };
+}
+
+const initialState = {
+  count: 0,
+  messages: []
+};
+
+export default (state = initialState, action) => {
+  const { type, username, content, count } = action;
+
+  if(type === CHAT_ADD_MESSAGE) {
+    return {
+      ...state,
+      messages: [ ...state.messages, {
+        type: 'SENT_MESSAGE',
+        username,
+        content
+      }]
+    };
+  }
+
+  if(type === CHAT_ADD_NOTIFICATION) {
+    return {
+      ...state,
+      messages: [ ...state.messages, {
+        type: 'SENT_NOTIFICATION',
+        content
+      }]
+    };
+  }
+
+  if(type === CHAT_SET_USER_COUNT) {
+    return {
+      ...state,
+      count
+    };
+  }
+
+
+  return state;
+};
