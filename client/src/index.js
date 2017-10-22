a66a0119f0ec87b3d9caa8016d93f43b2e2e2714
Karl Jensen <karljensen@gmail.com>
2017-10-21 18:15:29 -0700

    Setup redux with react.
    - Reducers for user and chat.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from 'app/redux/';
+
 import Root from 'app/containers/Root.jsx';
 
-render(<Root />, document.getElementById('app'));
+const store = createStore(reducer);
+
+const Application = () => (
+  <Provider store={ store }>
+    <Root />
+  </Provider>
+);
+
+render(<Application />, document.getElementById('app'));
---
+++ client/src/redux/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import user from './user';
+import chat from './chat';
+
+export default combineReducers({
+  user,
+  chat
+});
